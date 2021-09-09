9cfa1b3fdd84b266f4f75f27c8aa496f76bc31c1
Vulcan <vulcan.lee@gmail.com>
2021-09-09 17:09:03 +0800

    元件傳遞資料 -外層變化，通知內層 (使用參數傳遞)

---
+++ Examples/bz02/bz02/ClassArgument.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace bz02
+{
+    public class ClassArgument
+    {
+        public int MyInt { get; set; }
+        public string MyString { get; set; }
+        public DateTime MyDatetime { get; set; }
+
+        public override string ToString()
+        {
+            return $"({MyInt} - {MyString} - {MyDatetime})";
+        }
+    }
+}
